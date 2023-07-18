d60ad7cbfa6812a94acaf05d4ee1e797b0a54fa8
Rhynchium <james8486g@gmail.com>
2023-07-18 22:32:37 +0800

    START

---
+++ AJAX_Homework/Controllers/ApiController.cs
@@ -0,0 +1,59 @@
+﻿using AJAX_Homework.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace AJAX_Homework.Controllers
+{
+    public class ApiController : Controller
+    {
+        private readonly DemoContext _context;
+        public ApiController(DemoContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public IActionResult CheckAccount(Members member)
+        {
+            var datas = _context.Members.Select(a => a.Name).ToList();
+
+            if (datas.FirstOrDefault(m => m == member.Name) != null)
+            {
+                return Content("1");
+            }
+            return Content("");//回傳空字串作為false讀取
+        }
+
+        public IActionResult Cities()
+        {
+            var datas = _context.Address.Select(a => new
+            {
+                a.City
+            }).Distinct().OrderBy(a => a.City);
+
+            return Json(datas);
+        }
+
+        public IActionResult Sites(string city)
+        {
+            var datas = _context.Address.Where(a => a.City == city)
+                                        .Select(a => a.SiteId)
+                                        .Distinct().OrderBy(a => a);
+
+            return Json(datas);
+        }
+
+        public IActionResult Roads(string siteid)
+        {
+            var datas = _context.Address.Where(a => a.SiteId == siteid)
+                                        .Select(a => a.Road)
+                                        .Distinct().OrderBy(a => a);
+
+            return Json(datas);
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ AJAX_Homework/Controllers/HomeController.cs
@@ -0,0 +1,72 @@
+﻿using AJAX_Homework.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
+
+namespace AJAX_Homework.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        private readonly DemoContext _context;
+        private readonly IWebHostEnvironment _host;
+        public HomeController(ILogger<HomeController> logger, DemoContext context, IWebHostEnvironment host)
+        {
+            _logger = logger;
+            _context = context;
+            _host = host;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult First()
+        {
+            return View();
+        }
+
+        public IActionResult Second()
+        {
+            return View();
+        }
+
+        public IActionResult Third()
+        {
+            return View();
+        }
+
+        public IActionResult Fourth()
+        {
+            return View();
+        }
+
+        public IActionResult Fifth()
+        {
+            return View();
+        }
+
+        public IActionResult Sixth()
+        {
+            return View();
+        }
+
+        public IActionResult Seventh()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
